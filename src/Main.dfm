5c95e5839c8051102ee8cd020446d41146da5c2a
SnowSonic <pavelmoroz@gmail.com>
2019-08-02 09:42:05 +0300

    Первая загрузка

---
+++ src/Main.dfm
@@ -0,0 +1,79 @@
+object fmMain: TfmMain
+  Left = 0
+  Top = 0
+  Margins.Left = 2
+  Margins.Top = 2
+  Margins.Right = 2
+  Margins.Bottom = 2
+  BorderIcons = []
+  BorderStyle = bsNone
+  Caption = 'fmMain'
+  ClientHeight = 26
+  ClientWidth = 390
+  Color = clYellow
+  Font.Charset = DEFAULT_CHARSET
+  Font.Color = clWindowText
+  Font.Height = -11
+  Font.Name = 'Tahoma'
+  Font.Style = []
+  OldCreateOrder = False
+  Position = poScreenCenter
+  ScreenSnap = True
+  OnCreate = FormCreate
+  OnDestroy = FormDestroy
+  PixelsPerInch = 96
+  TextHeight = 13
+  object Splitter: TSplitter
+    Left = 285
+    Top = 0
+    Width = 5
+    Height = 26
+    Align = alRight
+    Color = clRed
+    ParentColor = False
+  end
+  object edEQ: TEdit
+    Left = 0
+    Top = 0
+    Width = 285
+    Height = 26
+    Align = alClient
+    AutoSize = False
+    BorderStyle = bsNone
+    Color = clBlack
+    Font.Charset = DEFAULT_CHARSET
+    Font.Color = clLime
+    Font.Height = -19
+    Font.Name = 'Tahoma'
+    Font.Style = []
+    ParentFont = False
+    TabOrder = 0
+    OnChange = edEQChange
+    OnKeyDown = edEQKeyDown
+  end
+  object stResult: TStaticText
+    Left = 290
+    Top = 0
+    Width = 100
+    Height = 26
+    Hint = 
+      #1057#1080#1085#1077#1077' '#1087#1086#1083#1077' - '#1088#1077#1079#1091#1083#1100#1090#1072#1090'.'#13#1047#1072' '#1085#1077#1075#1086' '#1084#1086#1078#1085#1086' '#1087#1077#1088#1077#1084#1077#1097#1072#1090#1100'.'#13'Ctrl '#1080' '#1097#1077#1083#1095#1086#1082' ' +
+      #1084#1099#1096#1082#1086#1081' '#1080' '#1082#1072#1083#1100#1082#1091#1083#1103#1090#1086#1088' '#1079#1072#1082#1088#1086#1077#1090#1089#1103'.'
+    Align = alRight
+    Alignment = taCenter
+    AutoSize = False
+    Color = clBlue
+    Font.Charset = DEFAULT_CHARSET
+    Font.Color = clYellow
+    Font.Height = -19
+    Font.Name = 'Tahoma'
+    Font.Style = []
+    ParentColor = False
+    ParentFont = False
+    ParentShowHint = False
+    ShowHint = True
+    TabOrder = 1
+    Transparent = False
+    OnMouseDown = stResultMouseDown
+  end
+end
---
+++ src/Main.pas
@@ -0,0 +1,171 @@
+unit Main;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics, Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls,
+  ParseExpr, Vcl.Buttons, Vcl.ExtCtrls;
+
+type
+  TfmMain = class(TForm)
+    edEQ: TEdit;
+    stResult: TStaticText;
+    Splitter: TSplitter;
+    procedure FormCreate(Sender: TObject);
+    procedure FormDestroy(Sender: TObject);
+    procedure edEQChange(Sender: TObject);
+    procedure edEQKeyDown(Sender: TObject; var Key: Word; Shift: TShiftState);
+    procedure stResultMouseDown(Sender: TObject; Button: TMouseButton; Shift: TShiftState; X, Y: Integer);
+  private
+    EP        : TExpressionParser;
+    ExpIndex  : Integer;
+    Exp       : TStrings;
+    LastResult: Int64;
+    function CalcThis: boolean;
+    procedure FormMouseWheel(Sender: TObject; Shift: TShiftState; WheelDelta: Integer; MousePos: TPoint; var Handled: boolean);
+  public
+    { Public declarations }
+  end;
+
+var
+  fmMain: TfmMain;
+
+implementation
+
+uses
+  StrUtils;
+
+{$R *.dfm}
+
+function TfmMain.CalcThis: boolean;
+begin
+  try
+    EP.ClearExpressions;
+    if (FormatSettings.DecimalSeparator = ',') then
+      EP.AddExpression(ReplaceStr(edEQ.Text, '.', ','))
+    else
+      EP.AddExpression(ReplaceStr(edEQ.Text, ',', '.'));
+    LastResult := Round(EP.AsFloat[0] * 100);
+    Result     := True;
+  except
+    Result := False;
+  end;
+end;
+
+procedure TfmMain.FormCreate(Sender: TObject);
+begin
+  EP           := TExpressionParser.Create;
+  Exp          := TStringList.Create;
+  ExpIndex     := - 1;
+  OnMouseWheel := FormMouseWheel;
+  { TODO : „тение полседнего расположени€ и размеров }
+  {
+  ѕрочитать данные о последнем положении и размере из реестра.
+  ≈сли прочитались, то применить к форме.
+  ≈сли впервые запускаетс€, то сделать ширину окна Width * 15 и расположить по центру
+  }
+end;
+
+procedure TfmMain.FormDestroy(Sender: TObject);
+begin
+  EP.Free;
+  Exp.Free;
+  { TODO : —охранение размеров и положени€ }
+end;
+
+procedure TfmMain.FormMouseWheel(Sender: TObject; Shift: TShiftState; WheelDelta: Integer; MousePos: TPoint; var Handled: boolean);
+  function Min(Val1, Val2: Integer): Integer;
+  begin
+    if Val1 < Val2 then
+      Result := Val1
+    else
+      Result := Val2
+  end;
+  function Max(Val1, Val2: Integer): Integer;
+  begin
+    if Val1 > Val2 then
+      Result := Val1
+    else
+      Result := Val2
+  end;
+
+begin
+  if WheelDelta > 0 then
+    Width := Min(Width + Height, Screen.Width)
+  else
+    Width := Max(Width - Height, Height * 15);
+end;
+
+procedure TfmMain.edEQChange(Sender: TObject);
+begin
+  if CalcThis then
+  begin
+    stResult.Font.Color := clYellow;
+    stResult.Caption    := Format('%0.2f', [LastResult / 100]);
+  end
+  else
+  begin
+    if Length(Trim(edEQ.Text)) > 0 then
+    begin
+      stResult.Font.Color := clRed;
+      stResult.Caption := 'ERROR';
+    end
+    else
+      stResult.Caption := '';
+  end;
+end;
+
+procedure TfmMain.edEQKeyDown(Sender: TObject; var Key: Word; Shift: TShiftState);
+var
+  SUM, PDV: Int64;
+begin
+  if (Key = VK_RETURN) then
+  begin
+    if CalcThis then
+    begin
+      if (Exp.IndexOf(edEQ.Text) < 0) then
+      begin
+        Exp.Add(edEQ.Text);
+        ExpIndex := Exp.Count - 1;
+      end;
+      if (ssShift in Shift) then
+      begin
+        PDV       := Round(LastResult / 6);
+        SUM       := LastResult - PDV;
+        edEQ.Text := Format('%0.2f+%0.2f', [SUM / 100, PDV / 100]);
+      end
+      else
+        edEQ.Text := Format('%0.2f', [LastResult / 100]);
+      edEQ.SelectAll;
+    end;
+  end
+  else if (Key = VK_ESCAPE) then
+  begin
+    Key              := 0;
+    edEQ.Text        := '';
+    stResult.Caption := '';
+  end
+  else if (Key = VK_UP) then
+  begin
+    Key := 0;
+    { TODO : ѕрокрутить список последних выражений на шаг назад }
+  end
+  else if (Key = VK_DOWN) then
+  begin
+    Key := 0;
+    { TODO : ѕрокрутить список последних выражений на шаг вперед }
+  end;
+end;
+
+procedure TfmMain.stResultMouseDown(Sender: TObject; Button: TMouseButton; Shift: TShiftState; X, Y: Integer);
+begin
+  if not (ssCtrl in Shift) then
+  begin
+    ReleaseCapture;
+    SendMessage(fmMain.Handle, WM_SYSCOMMAND, 61458, 0);
+  end
+  else
+    Close;
+end;
+
+end.
